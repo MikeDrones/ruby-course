f27dee16c3de8f6708bf45a8d02310665cea2ae7
Mike Kennedy <mikekennedy00@gmail.com>
2017-01-03 23:04:57 +0000

    fixed navbar category creation for admin

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = User.create(username: "john", email: "john@example12.com", password: "password", admin: true)
+  end
+  
   test "get new category form and create category" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -13,14 +18,14 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
   
   test "invalid category submission results in failure" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
       post categories_path, category: {name: " "}
     end
     assert_template 'categories/new'
-      #assert_select 'div.panel-heading'
-      #assert_select('div.panel-body')
-      #end   fffuuuuuucccccccckkkkkkkkk  
-  end
+      #assert_select('div < panel-heading')
+      #assert_select('div < panel-body')
+    end  # fffuuuuuucccccccckkkkkkkkk  
 end
